674bbf49411b72f29ad27e407ee67d5b0a94a02b
chellis111 <abajoadesoye2@gmail.com>
2025-01-29 15:32:50 +0100

    upate app

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const config = require("./config.json");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
@@ -15,7 +14,7 @@ const { authenticateToken } = require("./utilities");
 const User = require("./models/user.model");
 const TravelStory = require("./models/travelStory.model");
 
-mongoose.connect(config.connectionString);
+mongoose.connect(process.env.DB_URI);
 
 const app = express();
 app.use(express.json());
@@ -376,5 +375,7 @@ app.get("/travel-stories/filter", authenticateToken, async (req, res) => {
   }
 });
 
-app.listen(8000);
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
 module.exports = app;
